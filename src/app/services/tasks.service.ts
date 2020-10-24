f60155467983d9fd2f215427de9432f86321a8f3
Walace Rodrigues <walace@Mac-mini-de-Walace.local>
2020-10-24 05:24:25 -0300

    feat(task): heroku

--- src/app/services/tasks.service.ts
+++ src/app/services/tasks.service.ts
@@ -9,15 +9,19 @@ export class TasksService {
   constructor(private httpClient: HttpClient) { }
 
   getList() {
-    return this.httpClient.get('http://localhost:3001');
+    //return this.httpClient.get('http://localhost:3001');
+    return this.httpClient.get('https://api-manager-task.herokuapp.com');
   }
   save(task) {
-    return this.httpClient.post('http://localhost:3001', task);
+    //return this.httpClient.post('http://localhost:3001', task);
+    return this.httpClient.post('https://api-manager-task.herokuapp.com', task);
   }
   update(id) {
-    return this.httpClient.put(`http://localhost:3001/${id}`, id);
+    //return this.httpClient.put(`http://localhost:3001/${id}`, id);
+    return this.httpClient.put(`https://api-manager-task.herokuapp.com/${id}`, id);
   }
   delete(id) {
-    return this.httpClient.delete(`http://localhost:3001/${id}`);
+    //return this.httpClient.delete(`http://localhost:3001/${id}`);
+    return this.httpClient.delete(`https://api-manager-task.herokuapp.com/${id}`);
   }
 }
